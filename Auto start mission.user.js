7ed1824c47c12b43d4bc39a83d0110fb75991482
Ander Liu <anderliu0216@gmail.com>
2025-10-29 00:05:20 +0800

    Add files via upload

---
+++ Auto start mission.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Auto start mission
+// @namespace    https://github.com/NDR0216/
+// @version      0.1
+// @description  automatically click "Start Misssion"
+// @author       NDR0216
+// @match        https://www.reddit.com/r/SwordAndSupperGame/comments/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        window.onurlchange
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    let intervalId;
+
+    function clickButton() {
+        const start = (document.querySelector('shreddit-devvit-ui-loader')
+                     ?.shadowRoot?.querySelector("devvit-surface")
+                     ?.shadowRoot?.querySelector("devvit-blocks-renderer")
+                     ?.shadowRoot?.querySelector("div>div>div"));
+        if (start) {
+            start.click();
+
+            clearInterval(intervalId);
+            // release our intervalId from the variable
+            intervalId = null;
+        }
+    }
+
+    intervalId ??= setInterval(clickButton, 1000);
+
+    if (window.onurlchange === null) {
+        // feature is supported
+        window.addEventListener('urlchange', (info) => {intervalId ??= setInterval(clickButton, 1000);});
+    }
+})();
